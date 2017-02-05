551d9c99d7abcf1fa0ff76fb0f4dcbcc20a9ab7d
lovelysusie <haonanshihaoren@126.com>
2017-02-05 15:59:08 +0800

    made changes

--- titanic_rf.py
+++ titanic_rf.py
@@ -8,13 +8,13 @@ Created on Sun Jan 22 15:12:56 2017
 import csv as csv
 import numpy as np
 
-csv_file_object = csv.reader(open('train.csv', 'rb')) 	# Load in the csv file
-header = csv_file_object.next() 						# Skip the fist line as it is a header
-data=[] 												# Create a variable to hold the data
+csv_file_object = csv.reader(open('/Users/Susie/Documents/titanic_exercise/train.csv', 'rb')) 	# Load in the csv file
+header = csv_file_object.next() 		# Skip the fist line as it is a header
+data=[] 						# Create a variable to hold the data
 
-for row in csv_file_object: 							# Skip through each row in the csv file,
-    data.append(row[0:]) 								# adding each row to the data variable
-data = np.array(data) 									# Then convert from a list to an array.
+for row in csv_file_object: 	      # Skip through each row in the csv file,
+    data.append(row[0:]) 			# adding each row to the data variable
+data = np.array(data) 				# Then convert from a list to an array.
 
 # Now I have an array of 12 columns and 891 rows
 # I can access any element I want, so the entire first column would
@@ -53,7 +53,7 @@ print 'Proportion of men who survived is %s' % proportion_men_survived
 # if male, then model that he did not survive (0)
 
 # First, read in test.csv
-test_file = open('test.csv', 'rb')
+test_file = open('/Users/Susie/Documents/titanic_exercise/test.csv', 'rb')
 test_file_object = csv.reader(test_file)
 header = test_file_object.next()
 
@@ -61,13 +61,125 @@ header = test_file_object.next()
 # Finally, loop through each row in the train file, and look in column index [3] (which is 'Sex')
 # Write out the PassengerId, and my prediction.
 
-predictions_file = open("gendermodel.csv", "wb")
+predictions_file = open("/Users/Susie/Documents/titanic_exercise/gender_model.csv", "wb")
 predictions_file_object = csv.writer(predictions_file)
 predictions_file_object.writerow(["PassengerId", "Survived"])	# write the column headers
-for row in test_file_object:									# For each row in test file,
-    if row[3] == 'female':										# is it a female, if yes then
+for row in test_file_object:							# For each row in test file,
+    if row[3] == 'female':								# is it a female, if yes then
         predictions_file_object.writerow([row[0], "1"])			# write the PassengerId, and predict 1
-    else:														# or else if male,
+    else:											# or else if male,
         predictions_file_object.writerow([row[0], "0"])			# write the PassengerId, and predict 0.
-test_file.close()												# Close out the files.
+test_file.close()									# Close out the files.
+predictions_file.close()
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 22 23:56:19 2017
+
+@author: Susie
+"""
+
+import csv as csv
+import numpy as np
+
+csv_file_object = csv.reader(open('/Users/Susie/Documents/titanic_exercise/train.csv', 'rb'))       # Load in the csv file
+header = csv_file_object.next()                             # Skip the fist line as it is a header
+data=[]                                                     # Create a variable to hold the data
+
+for row in csv_file_object:                 # Skip through each row in the csv file
+    data.append(row)                        # adding each row to the data variable
+data = np.array(data)                       # Then convert from a list to an array
+
+# In order to analyse the price column I need to bin up that data
+# here are my binning parameters, the problem we face is some of the fares are very large
+# So we can either have a lot of bins with nothing in them or we can just lose some
+# information by just considering that anythng over 39 is simply in the last bin.
+# So we add a ceiling
+fare_ceiling = 40
+# then modify the data in the Fare column to = 39, if it is greater or equal to the ceiling
+data[ data[0::,9].astype(np.float) >= fare_ceiling, 9 ] = fare_ceiling - 1.0
+
+fare_bracket_size = 10
+number_of_price_brackets = fare_ceiling / fare_bracket_size
+number_of_classes = 3                             # I know there were 1st, 2nd and 3rd classes on board.
+number_of_classes = len(np.unique(data[0::,2]))   # But it's better practice to calculate this from the Pclass directly:
+                                                  # just take the length of an array of UNIQUE values in column index 2
+
+
+# This reference matrix will show the proportion of survivors as a sorted table of
+# gender, class and ticket fare.
+# First initialize it with all zeros
+survival_table = np.zeros([2,number_of_classes,number_of_price_brackets],float)
+
+# I can now find the stats of all the women and men on board
+for i in xrange(number_of_classes):
+    for j in xrange(number_of_price_brackets):
+
+        women_only_stats = data[ (data[0::,4] == "female") \
+                                 & (data[0::,2].astype(np.float) == i+1) \
+                                 & (data[0:,9].astype(np.float) >= j*fare_bracket_size) \
+                                 & (data[0:,9].astype(np.float) < (j+1)*fare_bracket_size), 1]
+
+        men_only_stats = data[ (data[0::,4] != "female") \
+                                 & (data[0::,2].astype(np.float) == i+1) \
+                                 & (data[0:,9].astype(np.float) >= j*fare_bracket_size) \
+                                 & (data[0:,9].astype(np.float) < (j+1)*fare_bracket_size), 1]
+
+                                 #if i == 0 and j == 3:
+
+        survival_table[0,i,j] = np.mean(women_only_stats.astype(np.float))  # Female stats
+        survival_table[1,i,j] = np.mean(men_only_stats.astype(np.float))    # Male stats
+
+# Since in python if it tries to find the mean of an array with nothing in it
+# (such that the denominator is 0), then it returns nan, we can convert these to 0
+# by just saying where does the array not equal the array, and set these to 0.
+survival_table[ survival_table != survival_table ] = 0.
+
+# Now I have my proportion of survivors, simply round them such that if <0.5
+# I predict they dont surivive, and if >= 0.5 they do
+survival_table[ survival_table < 0.5 ] = 0
+survival_table[ survival_table >= 0.5 ] = 1
+
+# Now I have my indicator I can read in the test file and write out
+# if a women then survived(1) if a man then did not survived (0)
+# First read in test
+test_file = open('test.csv', 'rb')
+test_file_object = csv.reader(test_file)
+header = test_file_object.next()
+
+# Also open the a new file so I can write to it. 
+predictions_file = open("genderclassmodel.csv", "wb")
+predictions_file_object = csv.writer(predictions_file)
+predictions_file_object.writerow(["PassengerId", "Survived"])
+
+# First thing to do is bin up the price file
+for row in test_file_object:
+    for j in xrange(number_of_price_brackets):
+        # If there is no fare then place the price of the ticket according to class
+        try:
+            row[8] = float(row[8])    # No fare recorded will come up as a string so
+                                      # try to make it a float
+        except:                       # If fails then just bin the fare according to the class
+            bin_fare = 3 - float(row[1])
+            break                     # Break from the loop and move to the next row
+        if row[8] > fare_ceiling:     # Otherwise now test to see if it is higher
+                                      # than the fare ceiling we set earlier
+            bin_fare = number_of_price_brackets - 1
+            break                     # And then break to the next row
+
+        if row[8] >= j*fare_bracket_size\
+            and row[8] < (j+1)*fare_bracket_size:     # If passed these tests then loop through
+                                                      # each bin until you find the right one
+                                                      # append it to the bin_fare
+                                                      # and move to the next loop
+            bin_fare = j
+            break
+        # Now I have the binned fare, passenger class, and whether female or male, we can
+        # just cross ref their details with our survival table
+    if row[3] == 'female':
+        predictions_file_object.writerow([row[0], "%d" % int(survival_table[ 0, float(row[1]) - 1, bin_fare ])])
+    else:
+        predictions_file_object.writerow([row[0], "%d" % int(survival_table[ 1, float(row[1]) - 1, bin_fare])])
+
+# Close out the files
+test_file.close()
 predictions_file.close()
